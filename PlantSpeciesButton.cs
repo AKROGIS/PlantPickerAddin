d33e7eb459647294c2369c6d3395ef523f00b852
Regan Sarwas <regan_sarwas@nps.gov>
2011-06-01 13:40:49 -0800

    Initial Commit

---
+++ PlantSpeciesButton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace PlantPickerAddIn
+{
+    public class PlantSpeciesButton : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        public PlantSpeciesButton()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            //
+            //  TODO: Sample code showing how to access button host
+            //
+            ArcMap.Application.CurrentTool = null;
+        }
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+
+}
