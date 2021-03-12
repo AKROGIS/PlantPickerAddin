a2850d161b0e82a7dcc1f0978303ed13ea615ca9
Regan Sarwas <regan_sarwas@nps.gov>
2021-03-12 11:07:51 -0900

    Start a Pro version of the Addin

---
+++ Pro/SpeciesRank.cs
@@ -0,0 +1,87 @@
+﻿using ArcGIS.Core.CIM;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Catalog;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Extensions;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Layouts;
+using ArcGIS.Desktop.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantPickerAddin
+{
+    /// <summary>
+    /// Represents the ComboBox
+    /// </summary>
+    internal class SpeciesRank : ComboBox
+    {
+
+        private bool _isInitialized;
+
+        /// <summary>
+        /// Combo Box constructor
+        /// </summary>
+        public SpeciesRank()
+        {
+            UpdateCombo();
+        }
+
+        /// <summary>
+        /// Updates the combo box with all the items.
+        /// </summary>
+
+        private void UpdateCombo()
+        {
+            // TODO – customize this method to populate the combobox with your desired items  
+            if (_isInitialized)
+                SelectedItem = ItemCollection.FirstOrDefault(); //set the default item in the comboBox
+
+
+            if (!_isInitialized)
+            {
+                Clear();
+
+                //Add 6 items to the combobox
+                for (int i = 0; i < 6; i++)
+                {
+                    string name = string.Format("Item {0}", i);
+                    Add(new ComboBoxItem(name));
+                }
+                _isInitialized = true;
+            }
+
+
+            Enabled = true; //enables the ComboBox
+            SelectedItem = ItemCollection.FirstOrDefault(); //set the default item in the comboBox
+
+        }
+
+        /// <summary>
+        /// The on comboBox selection change event. 
+        /// </summary>
+        /// <param name="item">The newly selected combo box item</param>
+        protected override void OnSelectionChange(ComboBoxItem item)
+        {
+
+            if (item == null)
+                return;
+
+            if (string.IsNullOrEmpty(item.Text))
+                return;
+
+            // TODO  Code behavior when selection changes.    
+        }
+
+    }
+}
